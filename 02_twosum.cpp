7e29a8b931821809a222e24f809f0a8a93da108f
Sumit Jadhav <sumit10102006@gmail.com>
2026-05-14 00:33:13 +0530

    pow(x,n) solution

--- 02_twosum.cpp
+++ 02_twosum.cpp
@@ -1,21 +1,73 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int arr[]={1,2,3,4,5,19,6,7,6,5,4,};
+//     int size=sizeof(arr)/sizeof(int);
+//     int target=3;
+//     for (int i = 0; i < size; i++)
+//     {
+//         for (int j = i+1; j < size; j++)
+//         {
+//             if (arr[i]+arr[j]==target)
+//             {
+//                 cout<<i<<","<<j;
+//             }
+            
+//         }
+        
+//     }
+    
+//     return 0;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int arr[]={1,2,3,4,5,19,6,7,6,5,4,};
+//     int size=sizeof(arr)/sizeof(int);
+//     int target=3;
+//     for (int i = 0; i < size; i++)
+//     {
+//         for (int j = i+1; j < size; j++)
+//         {
+//             if (arr[i]+arr[j]==target)
+//             {
+//                 cout<<i<<","<<j;
+//             }
+            
+//         }
+        
+//     }
+    
+//     return 0;
+// }
+
+
 #include <iostream>
+
 using namespace std;
 int main(){
-    int arr[]={1,2,3,4,5,19,6,7,6,5,4,};
-    int size=sizeof(arr)/sizeof(int);
-    int target=3;
-    for (int i = 0; i < size; i++)
+    // long nums=n;
+    
+    double x=2.5;
+    int n=5;
+    double ans=1;
+    if (n<0)
     {
-        for (int j = i+1; j < size; j++)
-        {
-            if (arr[i]+arr[j]==target)
-            {
-                cout<<i<<","<<j;
-            }
-            
-        }
-        
+        n=-n;
+        x=1/x;
     }
     
+    while(n>0){
+        if (n%2==1)
+        {
+            ans*=x;
+        }
+        n/=2;
+        x*=x;
+        
+    }
+    cout<<ans;
     return 0;
 }
